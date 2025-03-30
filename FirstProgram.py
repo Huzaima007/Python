a42db124e998a41b2910544d7604a1e27014463b
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-03-30 17:10:51 +0500

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,5 +1,53 @@
-name = "Huzaima"
-age = 16
-price = 100
-print(name,age,price)
-print("hello")
+# name = "Huzaima"
+# age = 16
+# price = 100
+# print(name,age,price)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# Arithmetic Operators 
+
+# a=5
+# b=9
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# print(a*b)
+
+# Relational Operators 
+
+# a = 50
+# b = 40
+
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a > b)
+# print(a <= b)
+# print(a < b) 
+
+# Assignment Operators
+
+# num = 10
+# # num = num + 10
+# num += 10;
+# num -= 10;
+# num *= 10;
+# num /= 10
+# num %= 10
+# num **= 10
+# print("num :" , num , num , num ,num ,num ,num)
+
+# Logical Operators
+
+a = 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = True 
+val2 = True
+print("ans operator:" , val1 and val2) 
