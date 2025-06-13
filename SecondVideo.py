2dde370827794cd9cd76805d4fb5ccac52055676
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-06-13 16:09:36 +0500

    Update SecondVideo.py

---
+++ SecondVideo.py
@@ -0,0 +1,28 @@
+# Strings
+
+# # In Next line 
+# str1 = "hlw.\nhehehe"
+# print(str1)
+
+
+# # Add Some Space 
+# str2 = "hlw.\thehehe"
+# print(str2)
+
+
+# Concatenation 
+
+# str1= "hlw.hehehe"
+# str2 = "hlwehehe"
+# laststr= str1+str2
+# print(laststr)
+
+
+# Length  
+
+# length1 = "hhhhhhhhhhhhhhhhh"
+# print(len(length1))
+
+# Indexing 
+# index starts form 0
+
