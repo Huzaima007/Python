194d8c409fc2573cb1d2ad12bb723b0bee877d18
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-06-25 15:58:56 +0500

    Update ThirdVideo.py

---
+++ ThirdVideo.py
@@ -0,0 +1,84 @@
+# Lists in PY / Array
+
+# marks = [90.5,80.5,70.5,60.5]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+
+# student = ["bazan",94.44,11,"karachi"]
+# print(student)
+
+
+# Difference b/w strings and lists in python 
+# mutable maens the thing that can be change
+# immutable maens the thing that can not be change
+# strings are immutable
+# lists are mutable
+
+# in this the h of the hello can not be change into y
+# str = "hello"
+# print(str[0])
+# str[0] = "y"
+
+
+# in this the name can be change from bazan to huzaima
+# student = ["bazan",94.44,11,"karachi"]
+# student[0] = "huzaima"
+# print(student)
+
+# is me jo ha jitni range ha us ma hi hum access kr sakte hain 5 ma ye error dede ga 
+# student = ["bazan",94.44,11,"karachi"]
+# print(student[5])
+
+
+# list slicing
+
+# positive slicing
+# marks = [90.5,80.5,70.5,60.5]
+# print(marks[1:3])
+ 
+# negative slicing 
+# marks = [90.5,80.5,70.5,60.5]
+# print(marks[-3:-1])
+
+# list method 
+
+# list = [1,2,3]
+# Append Method Is Also Called Mutating In The List 
+# Append Method / adds one element at the end
+# list.append(4)
+# print(list)
+
+# Sort Method Are In two Type Ascending And Descending Order
+
+# Ascending Sort Method 
+# print(list.sort())
+# print(list)
+
+# Descending Sort Method
+# print(list.sort(reverse=True))
+# print(list)
+
+# Reversed Method / reveses list
+# list.reverse()
+# print(list)
+
+# Insert Method / insert element at index
+# list.insert(2,5)
+# print(list)
+
+# Remove Method / removes first occurrence of element
+# list.remove(1)
+# print(list)
+
+# Pop Method / removes element at index 
+# list.pop(2)
+
+
+
+# Tuples In Python 
+
+# difference b/w tuple and in list, list are mutable ,tuples are immutable 
+
+
