a50a55bbc0c3a0efa6a259ceab90732a504380ac
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-06-28 16:56:48 +0500

    Update FifthVideo.py

--- FifthVideo.py
+++ FifthVideo.py
@@ -1,5 +1,43 @@
 # Loops In Python 
 
 # while Loop 
-# while True : 
+# count = 1
+# while count <= 5 : 
 #     print("hello")
+#     count += 1
+    
+    
+# count = 5
+# while count >= 1 : 
+#     print("hello")
+#     count -= 1
+
+# Practice While Loop 
+
+# Q1 
+
+# count = 1
+# while count <= 100 : 
+#     print("hello",count)
+#     count += 1
+
+
+# Q2 
+
+# count = 100
+# while count >= 1 : 
+#     print("hello", count)
+#     count -= 1
+
+
+# Q3 
+
+# mul = int(input("enter a number"))
+# i = 1 
+# while i <= 10 :
+#     print(mul*i)
+#     i += 1
+
+
+# Q4 
+
