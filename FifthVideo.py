89699be100f151ebc0a6e944471bcb5a5200804d
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-07-01 16:58:34 +0500

    Update FifthVideo.py

--- FifthVideo.py
+++ FifthVideo.py
@@ -41,4 +41,76 @@
 
 # Q4 
 
-print("Q4")
+# nums = [1,4,9,16,25,36,49,64,81,100]
+# i = 0
+# while i < len(nums):
+#     print(nums[i])
+#     i += 1
+
+# Q5 
+
+# nums = (1,4,9,16,25,36,49,64,81,100)
+
+# x = 36
+# i = 0
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("Found at idx" , i)
+#     else:
+#         print("finding....")    
+#     i += 1    
+
+
+
+# Break And continue In while loop 
+
+
+# Break : used to terminate the loop when encountered 
+
+# Continue : terminates execution in the current iteration & continues execution of the loop with the next iteration 
+
+
+# EX 1 Break
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if(i == 3):
+#         break
+#     i += 1
+    
+# print("loop break")
+
+
+# EX 2 Break
+# nums = (1,4,9,16,25,36,49,64,81,100)
+
+# x = 36
+# i = 0
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("Found at idx" , i)
+#         break
+#     else:
+#         print("finding....")    
+#     i += 1    
+# print("end of loop")
+
+
+
+# EX 3  Continue
+
+
+# i = 0
+# while i <= 5 :
+#     if(i == 3):
+#         i += 1
+#         continue  # skip
+#     print(i)
+#     i += 1
+
+
+
+# For Loop In Python 
+
+
