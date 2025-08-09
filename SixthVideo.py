d7d1c6b263d8e435fc0fba917171770692aa9268
Huzaima007 <160530387+Huzaima007@users.noreply.github.com>
2025-08-09 14:20:48 +0500

    functiuons

---
+++ SixthVideo.py
@@ -0,0 +1,39 @@
+#  Functions In Python 
+
+#  method 1 
+# def addition(a, b):
+#     sum = a + b
+#     print(sum)
+#     return(sum)
+
+# addition(5, 10)
+# addition(2, 10)
+# addition(12, 17)
+
+ 
+#  method 2 
+
+# def addition(a, b):
+#     return a + b
+
+# sum = addition(int(input("enter some num")),int(input("enter one more num")))
+# print(sum)
+
+
+
+# average of 3 numbers 
+
+# def calc_avg(a, b, c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# calc_avg(111, 222, 333)
+
+
+# Types Of function  
+
+# Built-in Functions 
+
+# User-defined Functions 
