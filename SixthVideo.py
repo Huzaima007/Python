9d333bf163c4c8547907eb29ee3ba3b58f7d3ef1
Huzaima007 <160530387+Huzaima007@users.noreply.github.com>
2025-09-17 14:35:41 +0500

    recursion

--- SixthVideo.py
+++ SixthVideo.py
@@ -122,3 +122,44 @@
 #     show(n-1)
 
 # show(10)
+
+
+
+# Returns n! 
+
+# def fact(n):
+#     if(n == 1 or n ==0):
+#         return 1
+#     return fact(n-1) * n 
+
+# print(fact(4))
+
+
+# Practice Ques
+
+
+
+# Q1
+
+# def calc_sum(n):
+#     if(n == 0):
+#         return 0 
+#     print(n)
+#     return calc_sum(n-1) + n
+ 
+# sum = calc_sum(5)
+# print(sum)
+
+
+
+# Q2
+
+def print_list(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+
+heros = ["mota" , "chota" , "kota" , "pota"]
+
+print_list(heros)
