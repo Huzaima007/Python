98d11918fc27f18ca088f9373057dd4f25902f4a
Huzaima007 <160530387+Huzaima007@users.noreply.github.com>
2025-09-23 15:49:47 +0500

    new

--- SeventhVideo/SeventhVideo.py
+++ SeventhVideo/SeventhVideo.py
@@ -7,6 +7,8 @@
 
 # READ ONLY METHOD
 
+# IT READS WHOLE FILE 
+
 
 # f = open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "r")
 # data = f.read()
@@ -25,7 +27,7 @@
 
 
 
-
+# IT ONLY READS ONE LINE 
 
 # READ ONLY LINE 1 
 
@@ -38,10 +40,47 @@
 
 # READ ONLY LINE 2
 
-f = open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "r")
-line1 = f.readline()
-print(line1)
+# f = open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "r")
+# line1 = f.readline()
+# print(line1)
 
-line2 = f.readline()
-print(line2)
-f.close()
+# line2 = f.readline()
+# print(line2)
+# f.close()
+
+
+
+
+# WRITE ONLY METHOD 
+
+
+
+# f = open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "w")
+
+# f.write("")
+# f.close()
+
+
+
+# WRITE AND READ METHOD 
+
+
+# f = open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "r+")
+
+# f.write("aaa")
+# f.close()
+
+
+# WITH SYNTAX 
+
+with open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "r") as f:
+    data = f.read()
+    print(data)
+
+
+with open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "w") as f:
+    f.write("new data ")
+
+
+
+# DELETING A FILE 
