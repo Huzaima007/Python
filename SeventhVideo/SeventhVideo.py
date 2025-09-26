5dce6847b1f98d9d6b6815c4e42da07a1b74011b
Huzaima007 <160530387+Huzaima007@users.noreply.github.com>
2025-09-26 14:56:51 +0500

    new work

--- SeventhVideo/SeventhVideo.py
+++ SeventhVideo/SeventhVideo.py
@@ -73,14 +73,139 @@
 
 # WITH SYNTAX 
 
-with open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "r") as f:
-    data = f.read()
-    print(data)
+# with open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "r") as f:
+#     data = f.read()
+#     print(data)
 
 
-with open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "w") as f:
-    f.write("new data ")
+# with open("G:\HUZAIMA_WORK\Python\SeventhVideo\demo.txt", "w") as f:
+#     f.write("new data ")
 
 
 
 # DELETING A FILE 
+
+# import os
+
+# os.remove("File name")
+
+
+
+
+# PRACTICE QUESTIONS 
+
+
+
+# QUES 1
+
+
+# with open("G:\HUZAIMA_WORK\Python\SeventhVideo\practice.txt", "w") as f:
+#     f.write("SAB BOLO HI\n")
+#     f.write("TUM SAB BOLO BYE")
+
+
+
+
+# QUES 2 
+
+
+# with open("G:\HUZAIMA_WORK\Python\SeventhVideo\practice.txt", "r") as f:
+#     data = f.read()
+
+# new_data = data.replace("SAB", "HUM")
+# print(new_data)
+
+# with open("G:\HUZAIMA_WORK\Python\SeventhVideo\practice.txt", "w") as f:
+#     f.write(new_data)
+
+
+
+
+# QUES 3 
+
+# word = "BOLO"
+# with open("G:\HUZAIMA_WORK\Python\SeventhVideo\practice.txt", "r") as f:
+#     data = f.read()
+#     if(data.find(word) != -1):
+#         print("Found")
+#     else:
+#         print("not found")
+
+
+
+
+
+
+# Wrap Ques 3 In A Function 
+
+
+# def check_for_word():
+#     word = "BOLO"
+#     with open("G:\HUZAIMA_WORK\Python\SeventhVideo\practice.txt", "r") as f:
+#         data = f.read()
+#         if(data.find(word) != -1):
+#            print("Found")
+#         else:
+#             print("not found")
+
+# check_for_word()
+
+
+
+
+
+
+# QUES 4  
+
+
+# def check_for_line():
+#     word = "BOLO"
+#     data = True
+#     line_no = 1
+#     with open("G:\HUZAIMA_WORK\Python\SeventhVideo\practice.txt", "r") as f:
+#         while data:
+#             data = f.readline()
+#             if(word in data):
+#                 print(line_no)
+#                 return
+#             line_no += 1
+
+#     return -1
+
+# check_for_line()
+
+
+
+
+# QUES 5 
+
+
+
+#  METHOD 1 
+
+
+# with open("G:\HUZAIMA_WORK\Python\SeventhVideo\practice.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+
+#     num = ""
+#     for i in range(len(data)):
+#         if(data[i] == ","):
+#             print(int(num))
+#             num = ""
+#         else:
+#             num += data[i]
+
+
+#  METHOD 2 
+
+count = 0
+with open("SeventhVideo\practice.txt", "r") as f:
+    data = f.read()
+
+    nums = data.split(",")
+    for val in nums:
+        if(int(val) % 2 == 0):
+            count += 1
+
+print(count)
