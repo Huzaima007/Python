5db178bed48f330b1a7109eaea10464e27d2b6b9
Huzaima007 <160530387+Huzaima007@users.noreply.github.com>
2025-10-03 14:35:21 +0500

    oop

---
+++ EightVideo.py
@@ -0,0 +1,32 @@
+# OOP In Python  
+# oop sf object oriented programming 
+
+
+
+
+# Class And Object In Python
+
+
+# class Student:
+#     name = "Huzaima"
+
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s2.name)
+
+
+
+
+# class Car:
+#     color = "Red"
+#     brand = "BMW"
+
+# car1 = Car()
+# print(car1.color,car1.brand)
+    
+
+
+# Constructor 
+
