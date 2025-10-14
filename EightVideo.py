ca5b3e2ba9aacd6f287849120d5a472aabfae7aa
Huzaima007 <160530387+Huzaima007@users.noreply.github.com>
2025-10-14 13:32:11 +0500

    new things

--- EightVideo.py
+++ EightVideo.py
@@ -27,6 +27,90 @@
 # print(car1.color,car1.brand)
     
 
+# INIT FUNCTION 
 
 # Constructor 
+# VERSION 1 
+# class Student:
+# parameterized constructors 
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#         print("adding new student in database")
+
+# s1 = Student("HUZAIMA" , 100)
+# print(s1.name, s1.marks)
+
+# s2 = Student("KARAN" , 90)
+# print(s2.name, s2.marks)
+
+
+# VERSION 2 
+
+
+# class Student:
+#     # default constructor 
+#     def __init__(self):
+#         pass
+
+# s1 = Student("HUZAIMA" , 100)
+# print(s1.name, s1.marks)
+
+# s2 = Student("KARAN" , 90)
+# print(s2.name, s2.marks)
+
+
+
+# CLASS & INSTANCE ATTRIBUTES 
+
+
+# class Student:
+
+#     college_name = "ABC"
+
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#         print("adding new student in database")
+
+# s1 = Student("HUZAIMA" , 100)
+# print(s1.name, s1.marks)
+
+# s2 = Student("KARAN" , 90)
+# print(s2.name, s2.marks)
+
+# print(Student.college_name)
+
+
+
+
+
+
+
+
+
+
+
+
+# class Student:
+
+#     college_name = "ABC"
+#     name = "anonymous"
+
+
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#         print("adding new student in database")
+
+# s1 = Student("HUZAIMA" , 100)
+# print(s1.name)
+
+
+
+
+
+
+# METHODS 
+
 
